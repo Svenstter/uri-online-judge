f0f5dccf79bec5305dea4f7b83592a07d40f9e4e
Svenstter <56422070+Svenstter@users.noreply.github.com>
2019-10-11 00:34:27 -0300

    Update 1002-Area_of_a_Circle.c

--- beginner/c/1002-Area_of_a_Circle.c
+++ beginner/c/1002-Area_of_a_Circle.c
@@ -1,7 +1,7 @@
 
 /*  ---------------------------------------------------------------------------------
     Problem
-	The formula to calculate the area of a circumference is defined as A = π . R2.
+    The formula to calculate the area of a circumference is defined as A = π . R2.
     Considering to this problem that π = 3.14159:
 
     Calculate the area using the formula given in the problem description.
