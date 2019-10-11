d08580ce039c17859e1caac2382414a142393f54
Svenstter <svenstter@gmail.com>
2019-10-11 00:29:08 -0300

    1001 - Extremely Basic - C++

---
+++ beginner/cpp/1001-Extremely_Basic.cpp
@@ -0,0 +1,35 @@
+
+/*	---------------------------------------------------------------------------------
+	Problem
+	Read 2 variables, named A and B and make the sum of these two variables,
+	assigning its result to the variable X. Print X as shown below.
+	Print endline after the result otherwise you will get “Presentation Error”.
+
+	Input
+	The input file will contain 2 integer numbers.
+
+	Output
+	Print the letter X (uppercase) with a blank space before and after the equal
+	signal followed by the value of X, according to the following example.
+
+	Obs.: don't forget the endline after all.
+	---------------------------------------------------------------------------------  */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio( false );
+
+	int A, B, X;
+
+	cin >> A >> B;
+
+	X = A + B;
+
+	cout << "X = " << X << endl;
+
+	system("PAUSE");
+    return EXIT_SUCCESS;
+}
