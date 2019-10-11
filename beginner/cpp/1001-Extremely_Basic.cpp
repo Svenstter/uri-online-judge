be09ecf05ea027a81b848003df016905dacd3ea4
Svenstter <56422070+Svenstter@users.noreply.github.com>
2019-10-11 00:36:26 -0300

    Update 1001-Extremely_Basic.cpp

--- beginner/cpp/1001-Extremely_Basic.cpp
+++ beginner/cpp/1001-Extremely_Basic.cpp
@@ -1,5 +1,5 @@
 
-/*    ---------------------------------------------------------------------------------
+/*  ---------------------------------------------------------------------------------
     Problem
     Read 2 variables, named A and B and make the sum of these two variables,
     assigning its result to the variable X. Print X as shown below.
